bbf8118e66a9f360f7bce389bc65ea50de91d721
Huteng Dai <hutengdai@gmail.com>
2019-09-20 11:45:22 -0400

    ok

--- similarity.py
+++ similarity.py
@@ -60,8 +60,6 @@ def simiarity(segment1, segment2,total_feature,dlist):
     m = 0
     n = 0
     shared_features = []
-    unshared_features_a = []
-    unshared_features_b = []
     a = segsFeats.get(segment1)
     b = segsFeats.get(segment2)
     # print("There are "+str(len(a))+" features in a")
@@ -71,20 +69,47 @@ def simiarity(segment1, segment2,total_feature,dlist):
                 m+=1
                 shared_features.append(feature_a)
             else:
-
                 n+=1
-    # print(len(shared_features))
-    unshared_features_a = np.setdiff1d(a,shared_features)
-    unshared_features_b = np.setdiff1d(b,shared_features)
-    #pair each feature class with a weight
-    # print("shared features include "+str(shared_features))
-    # print("unshared features in first segment include "+ str(unshared_features_a))
-    # print("unshared features in second segment include "+ str(unshared_features_b))
-    # for unshared_f in unshared_features_a:
+
+    unshared_feature_value_a = np.setdiff1d(a,shared_features).tolist()
+    unshared_feature_value_b = np.setdiff1d(b,shared_features).tolist()
+
+    unshared_features_raw = []
+    unshared_features_final = []
+    for feature_value_a in unshared_feature_value_a:
+        for feature_value_b in unshared_feature_value_b:
+            unshared_features_raw.append(feature_value_a[1:])
+            unshared_features_raw.append(feature_value_b[1:])
+            
+            for x in unshared_features_raw:
+                if x not in unshared_features_final:
+                    unshared_features_final.append(x)
+
+
+    def activation(unshared_features_final):
+        activation = 0
+        for unsharedFeature in unshared_features_final:
+            try:
+                activation += dlist[unsharedFeature]
+            except:
+                pass
+        return activation
+
+
+    similarity = m/total_feature - activation(unshared_features_final)
     
-    similarity = m/total_feature 
-    return similarity, unshared_features_a, unshared_features_b
-                
+    return similarity
+
+def writedataframe(consonant,total_feature,dlist):
+    similarity_etc = {}
+    df = pd.DataFrame(index=consonant, columns=consonant)
+    df = df.fillna(0)
+    for x in consonant:
+        for y in consonant:
+            similarity_etc[y] = simiarity(x,y,total_feature,dlist)
+        df.loc[x]= pd.Series(similarity_etc)
+        df.to_csv(r'similarity-matrix.csv')
+    return df                
                 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -104,38 +129,22 @@ if __name__ == '__main__':
         feat_file = featurefile.readlines()
         features = feat_file.pop(0).lstrip("\t").rstrip("\n").split("\t")
         segsFeats = segsFeats(filepath)  
-    # print(len(features))
-    # dlist = {'+cg': 0.4,
-    #          '-cg': 0.3,
-    #          '0cg': 0.2,
-    #          '+sg': 0.2,
-    #          '-sg': 0.1,
-    #          '0sg': 0.2,
-    #          }       
-    dlist = {'+cg': 0.4,
-             '-voice': 0.3,
-             '+sg': 0.2,
-             '+voice': 0.1
+
+    dlist = {'cg' : 0.1, 
+            'voice' : 0.2,
+            'sg' : 0.3,
              }    
     # consoant = [p,t,k,q,ʔ,b,d,ɡ,t͡s,t͡ʃ,f,s,ʃ,x,χ,h,z,ʒ,ʁ,m,n,r,j,l,w,i,y,u,e,ӕ,a,pʰ,pʼ,tʼ,tʰ,tʰʷ,tʷʼ,tʷ,t͡sʰ,t͡sʼ,t͡sʰʷ,t͡sʷʼ,t͡sʷ,sʷ,zʷ,t͡ʃʼ,t͡ʃʰ,kʰ,kʼ,kʷ,kʷʼ,kʰʷ,ɡʷ,χʷ,qʼ,qʰ,qʷ,qʷʼ,qʰʷ,ʁʷ]
     consonant = list(segsFeats.keys())
-    similarity_etc = {}
-    
-    df = pd.DataFrame(index=consonant, columns=consonant)
-    df = df.fillna(0)
-    for x in consonant:
-        for y in consonant:
-            m_tuple = simiarity(x,y,28,dlist)
-            similarity_etc[y] = m_tuple[0]
-        df.loc[x]= pd.Series(similarity_etc)
-    print(df)
-    df.to_csv(r'similarity-matrix.csv')
 
-    
-    # print(simiarity('p','p',28,dlist))
+    df = writedataframe(consonant,28,dlist)
+    print(df)
+    # print(simiarity('p',"pʼ", 28, dlist)) #0.1
+    # print(simiarity('p',"b", 28, dlist)) #0.2
+    # print(simiarity('p',"pʰ", 28, dlist)) #0.3
+
     # print(similarity_etc)
-    # unshared_features_a=similarity_etc[1]
-    # unshared_features_b=similarity_etc[2]
+
     
     # for f_a in unshared_features_a:
     #     for f_b in unshared_features_b:
